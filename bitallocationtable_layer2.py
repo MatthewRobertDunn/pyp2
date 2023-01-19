e23047f04fa511b4b8296aaf1d61a7be59b7d37a
Matthew <mrdunn@gmail.com>
2023-01-19 15:13:35 +1030

    Update formatting

--- bitallocationtable_layer2.py
+++ bitallocationtable_layer2.py
@@ -1,11 +1,13 @@
 from enum import Enum
 from typing import List
 
+
 class SampleBits:
     '''
         Describes how a channel sample is stored
         bits: number of bits used to describe a sample, if bits < 0 then it implies grouped, see mpeg spec
     '''
+
     def __init__(self,  bits: int) -> None:
         if bits < 0:
             self.grouped = True
@@ -21,6 +23,7 @@ class AllocationRow:
         allocation_bits: Number of bits used to describe how many bits per sample
         sample_bits: Number of bits to describe a sample for a given configuration of allocation_bits 
     '''
+
     def __init__(self, allocation_bits: int, sample_bits: List[int]) -> None:
         self.allocation_bits = allocation_bits
         self.sample_bits = [SampleBits(bit) for bit in sample_bits]
@@ -31,23 +34,25 @@ Number of bits to describe channel sample storage, followed by actual number of 
 Negative means 'grouped' samples. See mpeg spec
 '''
 AllocationTableRows = [
-        AllocationRow(2, [0, -5, -7, 16]),
-        AllocationRow(3, [0, -5, -7,  3,-10,  4,  5, 16]),
-        AllocationRow(4, [0, -5, -7,  3,-10,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 16]),
-        AllocationRow(4, [0, -5,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16]),
-        AllocationRow(4, [0, -5, -7,-10,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15]),
-        AllocationRow(3, [ 0, -5, -7,-10,  4,  5,  6,  9]),
-        AllocationRow(4, [0, -5, -7,  3,-10,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14]),
-        AllocationRow(2, [0, -5, -7,  3])
+    AllocationRow(2, [0, -5, -7, 16]),
+    AllocationRow(3, [0, -5, -7,  3, -10,  4,  5, 16]),
+    AllocationRow(4, [0, -5, -7,  3, -10,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 16]),
+    AllocationRow(4, [0, -5,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16]),
+    AllocationRow(4, [0, -5, -7, -10,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15]),
+    AllocationRow(3, [0, -5, -7, -10,  4,  5,  6,  9]),
+    AllocationRow(4, [0, -5, -7,  3, -10,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14]),
+    AllocationRow(2, [0, -5, -7,  3])
 ]
 
 '''
 The actual allocation lookup table. This is the only part of this useful to consumers of this module
 One of these enum values needs to be chosen based on bitrate, sample rate, mpeg version.
 '''
+
+
 class AllocationTable(Enum):
     high_rate_27_bands = [AllocationRow[x] for x in [3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0]]
-    high_rate_30_bands = [AllocationRow[x] for x in [3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 ]]
+    high_rate_30_bands = [AllocationRow[x] for x in [3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]]
     low_rate_7_bands = [AllocationRow[x] for x in [4, 4, 5, 5, 5, 5, 5, 5]]
-    #For mpeg2.5? don't think this gets used.
-    lsf_30_bands = [AllocationRow[x] for x in [ 6, 6, 6, 6, 5, 5, 5, 5, 5, 5, 5, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7]],
+    # For mpeg2.5? don't think this gets used.
+    lsf_30_bands = [AllocationRow[x] for x in [6, 6, 6, 6, 5, 5, 5, 5, 5, 5, 5, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7]],
